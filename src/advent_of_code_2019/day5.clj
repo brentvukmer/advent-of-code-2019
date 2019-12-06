327704c1da2a1480242193da0518698cc298a8d4
brent-vukmer <brent.vukmer@capitalone.com>
2019-12-06 16:46:28 -0500

    Day 5 Part 2 .. still busted.

--- src/advent_of_code_2019/day5.clj
+++ src/advent_of_code_2019/day5.clj
@@ -1,7 +1,10 @@
 (ns advent-of-code-2019.day5
-  (:require [advent-of-code-2019.day2 :as intcode]
-            [advent-of-code-2019.day4 :as day4]
+  (:require [advent-of-code-2019.day4 :as day4]
             [clojure.java.io :as io]))
+
+;;
+;; https://adventofcode.com/2019/day/5
+;;
 
 (defn read-and-save
   [v write-index]
@@ -14,13 +17,76 @@
   [input]
   (println (str input)))
 
-(def test-opcodes (assoc intcode/opcodes
+(defn param-val
+  [mode v val]
+  (if (= :immediate mode)
+    val
+    (get v val)))
+
+(defn- jump-if
+  [v instruction-index params param-modes f]
+  (let [[p1 p2] params
+        [m1 m2] param-modes
+        val1 (param-val m1 v p1)
+        val2 (param-val m2 v p2)]
+    ;(println (str "jump-if: params = " (into [] params)))
+    ;(println (str "jump-if: param-modes = " param-modes))
+    ;(println (str "jump-if: val1 = " val1))
+    ;(println (str "jump-if: val2 = " val2))
+    (if (f val1)
+      [instruction-index
+       (assoc v instruction-index val2)]
+      [(+ instruction-index (inc (count params)))
+       v])))
+
+(defn jump-if-true
+  [v instruction-index params param-modes]
+  (jump-if v instruction-index params param-modes (complement zero?)))
+
+(defn jump-if-false
+  [v instruction-index params param-modes]
+  (jump-if v instruction-index params param-modes zero?))
+
+(defn- write-one-or-zero
+  [v instruction-index params param-modes f]
+  (let [[p1 p2 p3] params
+        [m1 m2 m3] param-modes
+        val1 (param-val m1 v p1)
+        val2 (param-val m2 v p2)
+        val3 (param-val m3 v p3)
+        store-val (if (f val1 val2) 1 0)
+        updated-v (assoc v val3 store-val)]
+    (if (= val3 instruction-index)
+      [instruction-index
+       updated-v]
+      [(+ instruction-index (inc (count params)))
+       updated-v])))
+
+(defn less-than
+  [v instruction-index params param-modes]
+  (write-one-or-zero v instruction-index params param-modes <))
+
+(defn equals
+  [v instruction-index params param-modes]
+  (write-one-or-zero v instruction-index params param-modes =))
+
+(def test-opcodes (assoc {1  +
+                          2  *
+                          99 :stop}
                     3 read-and-save
-                    4 output))
+                    4 output
+                    5 jump-if-true
+                    6 jump-if-false
+                    7 less-than
+                    8 equals))
 (defn num-opcode-params
   [opcode]
-  (if (contains? #{3 4} opcode)
+  (cond
+    (contains? #{3 4} opcode)
     1
+    (contains? #{5 6} opcode)
+    2
+    :else
     3))
 
 (def param-modes {0 :position
@@ -43,43 +109,43 @@
      :num-params  num-params
      :param-modes param-modes}))
 
-(defn param-val
-  [mode v index-or-val]
-  (if (= :immediate mode)
-    index-or-val
-    (get v index-or-val)))
-
 (defn run-op
-  [{:keys [opcode num-params param-modes]} program index]
-  (let [op (get test-opcodes opcode)
+  [{:keys [opcode num-params param-modes]} program index opcode-lookup]
+  (let [op (get opcode-lookup opcode)
         params (take num-params (drop (+ index 1) program))
-        next-index (+ index (inc num-params))]
-    ;(do
-    ;  (println (str "opcode = " opcode))
-    ;  (println (str "num-params = " num-params))
-    ;  (println (str "params = " (into [] params)))
-    ;  (println (str "param-modes = " param-modes))
-    ;  (println (str "program = " program))
-    ;  (println (str "index = " index)))
+        default-next-index (+ index (inc num-params))]
+    ;(println (str "run-op: opcode = " opcode))
+    ;(println (str "run-op: op = " op))
+    ;(println (str "run-op: num-params = " num-params))
+    ;(println (str "run-op: params = " (into [] params)))
+    ;(println (str "run-op: param-modes = " param-modes))
+    ;(println (str "run-op: program = " program))
+    ;(println (str "run-op: index = " index))
     (cond
       (contains? #{+ *} op)
       (let [[p1 p2 p3] params
             [m1 m2 _] param-modes
             result (op (param-val m1 program p1) (param-val m2 program p2))]
-        [next-index
+        [default-next-index
          (assoc program p3 result)])
 
       (= op read-and-save)
-      [next-index
+      [default-next-index
        (let [write-index (first params)]
-         (read-and-save program write-index))]
+         (op program write-index))]
 
       (= op output)
       (let [mode (first param-modes)
             val (param-val mode program (first params))]
-        (output val)
-        [next-index
-         program]))))
+        (op val)
+        [default-next-index
+         program])
+
+      (contains? #{jump-if-true
+                   jump-if-false
+                   less-than
+                   equals} op)
+      (op program index params param-modes))))
 
 (defn stop?
   [opcode-info]
@@ -87,26 +153,46 @@
     (= op :stop)))
 
 (defn run-test-program
-  [program]
+  [program opcode-lookup]
   (loop [index 0
          updated-program program]
-    ;(println (str "index =" index))
-    ;(println (str "updated-program =" updated-program))
+    ;(println (str "run-test-program: index =" index))
+    ;(println (str "run-test-program: updated-program =" updated-program))
     (let [opcode-input (last (take (+ index 1) updated-program))
           opcode-info (parse-opcode opcode-input)]
       (if (stop? opcode-info)
         updated-program
-        (let [[latest-index latest-program] (run-op opcode-info updated-program index)]
+        (let [[latest-index latest-program] (run-op opcode-info updated-program index opcode-lookup)]
           (recur latest-index latest-program))))))
 
-(def test-program-1 [3,0,4,0,99])
+(def part1-test-input-1 [3, 0, 4, 0, 99])
 
-(def test-program-2 [1002,4,3,4,33])
+(def part1-test-input-2 [1002, 4, 3, 4, 33])
+
+(def part2-test-input1 [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8])
+
+(def part2-test-input2 [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8])
+
+(def part2-test-input3 [3, 3, 1108, -1, 8, 3, 4, 3, 99])
+
+(def part2-test-input4 [3, 3, 1107, -1, 8, 3, 4, 3, 99])
+
+(def part2-test-input5 [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9])
+
+(def part2-test-input6 [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1])
+
+(def part2-test-input7 [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                        1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                        999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99])
 
 (def input (read-string (str "[" (slurp (clojure.java.io/resource "day5")) "]")))
 
-(defn part1
-  ([program]
-   (run-test-program program))
-  ([]
-   (run-test-program input)))
+(comment
+  ; Part 1
+  ; (Enter 1)
+  (run-test-program input test-opcodes)
+
+  ; Part 2
+  ; (Enter 5)
+  (run-test-program input test-opcodes))
+
