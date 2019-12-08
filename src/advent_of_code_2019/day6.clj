236c10f42763a7e39f08ca16efd23174d6331fdf
Brent Vukmer <brent.vukmer@gmail.com>
2019-12-07 19:27:14 -0500

    Solved Day 6 Part 2

--- src/advent_of_code_2019/day6.clj
+++ src/advent_of_code_2019/day6.clj
@@ -1,6 +1,14 @@
 (ns advent-of-code-2019.day6
   (:require [ubergraph.core :as uber]))
 
+;;
+;; https://adventofcode.com/2019/day/6
+;;
+
+;;
+;; PART 1
+;;
+
 (defn parse-input
   [fn]
   (->> (slurp (clojure.java.io/resource fn))
@@ -25,12 +33,6 @@
 
 (def part1-graph (make-directed-graph input))
 
-;
-; Get all objects except COM
-; For each object, recursively build path to COM
-; Sum the lengths of all paths
-;
-
 (defn collect-paths
   [input]
   (let [objs (set (apply concat input))
@@ -51,3 +53,42 @@
         (apply +)))
   ([]
    (part1 input)))
+
+;;
+;; PART 2
+;;
+
+(def part2-test-input (parse-input "day6-part2-test"))
+
+(def part2-test-graph (make-directed-graph part2-test-input))
+
+(defn find-path
+  [start end g]
+  (loop [path []
+         node start]
+    (if (= node end)
+      path
+      (recur (conj path node)
+             (->> (uber/find-edge g {:src node})
+                  (uber/dest))))))
+
+(defn find-shortest-hop
+  [g]
+  (let [you-path (find-path :YOU :COM g)
+        san-path (find-path :SAN :COM g)
+        [common-y common-s] (loop [y you-path
+                              s san-path]
+                         (if (not= (peek y) (peek s))
+                           [y s]
+                           (recur (pop y)
+                                  (pop s))))]
+    (-> (conj common-y (-> (uber/find-edge g {:src (last common-y)})
+                           uber/dest))
+        (concat (reverse common-s)))))
+
+(defn part2
+  ([g]
+   (let [c (count (find-shortest-hop g))]
+     (dec (- c 2))))
+  ([]
+   (part2 part1-graph)))
